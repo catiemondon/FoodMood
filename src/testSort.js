6484de8e15f01f30b4b3d8cb9efd817967d3d4fb
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-08-02 14:52:21 -0400

    adds code for potential sorting solution

--- src/testSort.js
+++ src/testSort.js
@@ -12,3 +12,17 @@ const points = [
 const origin = {x:-111.9033339 ,y:40.7557073 }
 
 console.log(sortByDistance(origin, points))
+
+// [ { x: -111.8924901,
+// y: 40.7560063,
+// distance: 0.010847921434084936 },
+// { x: -111.8911626, y: 40.753798, distance: 0.012320144892821483 },
+// { x: -111.8932018,
+// y: 40.7335657,
+// distance: 0.024349741291644914 } ]
+
+// Sort by distance return an array of objects. 
+// We could use the index of the array to return dishes in a specific order
+// 
+//
+// 
